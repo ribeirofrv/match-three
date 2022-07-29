f9ca1a19d40cb7f6808cfd17975e704828ea7c73
Fran Ribeiro <ribeirofrv@gmail.com>
2022-07-28 23:40:21 -0300

    feat: generate block frame randomly

--- scripts/grid.gd
+++ scripts/grid.gd
@@ -6,11 +6,21 @@ export (int) var x_start;
 export (int) var y_start;
 export (int) var offset;
 
+var possible_pieces = [
+preload("res://scenes/blue_piece.tscn"),
+preload("res://scenes/yellow_piece.tscn"),
+preload("res://scenes/pink_piece.tscn"),
+preload("res://scenes/orange_piece.tscn"),
+preload("res://scenes/light_green_piece.tscn"),
+preload("res://scenes/green_piece.tscn")
+];
+
 var all_pieces = [];
 
 func _ready():
+	randomize();
 	all_pieces = make_2d_array();
-	print(all_pieces);
+	spawn_pieces();
 
 func make_2d_array():
 	var array = [];
@@ -19,6 +29,16 @@ func make_2d_array():
 		for index_aux in heigth:
 			array[index].append(null);
 	return array;
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func spawn_pieces():
+	for column in width:
+		for row in heigth:
+			var random = floor(rand_range(0, possible_pieces.size()));
+			var piece  = possible_pieces[random].instance();
+			add_child(piece);
+			piece.position = grid_to_pixel(column, row);
+
+func grid_to_pixel(column, row):
+	var new_x = x_start + offset * column;
+	var new_y = y_start + (-offset) * row;
+	return Vector2(new_x, new_y);
